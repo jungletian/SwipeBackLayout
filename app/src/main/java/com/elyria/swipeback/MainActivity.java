d06a365043457c2e891d3fba16bbfaa78366346d
jungletian <tianjun.zhang@qunar.com>
2017-09-29 14:53:34 +0800

    complete :beers:

---
+++ app/src/main/java/com/elyria/swipeback/MainActivity.java
@@ -0,0 +1,155 @@
+package com.elyria.swipeback;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
+import android.widget.TextView;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "SWIPE";
+    private static final int DURATION = 300;
+    private TextView enableTextView;
+    private FrameLayout container;
+    private static int bgIndex = 0;
+    private boolean isAnimating = false;
+    private int pageIndex = 0;
+    private int[] bgColors;
+    private int screenWidth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        enableTextView = (TextView) findViewById(R.id.text_enable);
+        container = (FrameLayout) findViewById(R.id.container);
+        screenWidth = getResources().getDisplayMetrics().widthPixels;
+    }
+
+    public void startNewActivity(View view) {
+        SwipeBackLayout swipeBackLayout = new SwipeBackLayout(this);
+        Log.d(TAG, String.format("this is %s", swipeBackLayout));
+        final TextView textView = getTextView();
+        swipeBackLayout.setupReactRootView(textView);
+        swipeBackLayout.setSwipeBackEnable(true);
+        container.addView(swipeBackLayout);
+        updateEnableView();
+        swipeBackLayout.setSwipeCallback(new SwipeBackLayout.SwipeCallback() {
+            @Override
+            public void onShouldFinish() {
+                container.removeViewAt(pageIndex - 1);
+                pageIndex--;
+                updateEnableView();
+            }
+
+            @Override
+            public void onScrollPercent(float scrollPercent) {
+                Log.d(TAG, String.format("scrollPercent = %s", scrollPercent));
+            }
+
+            @Override
+            public void onScrollOverThreshold() {
+                Log.d(TAG, "onScrollOverThreshold");
+            }
+        });
+    }
+
+    private void updateEnableView() {
+        if (container.getChildCount() > 0) {
+            SwipeBackLayout swipeBackLayout = (SwipeBackLayout) container.getChildAt(pageIndex - 1);
+            enableTextView.setText(!swipeBackLayout.getSwipeBackEnable() ? R.string.enable : R.string.disable);
+        }
+        enableTextView.setVisibility(container.getChildCount() > 0 ? View.VISIBLE : View.GONE);
+    }
+
+    @NonNull
+    private TextView getTextView() {
+        final TextView textView = new TextView(this);
+        textView.setBackgroundColor(getColors()[bgIndex]);
+        bgIndex++;
+        if (bgIndex >= getColors().length) {
+            bgIndex = 0;
+        }
+        ValueAnimator enterAnimation = ValueAnimator.ofFloat(screenWidth, 0);
+        enterAnimation.setDuration(DURATION);
+        enterAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                textView.setTranslationX((Float) valueAnimator.getAnimatedValue());
+            }
+        });
+        textView.setGravity(Gravity.CENTER);
+        enterAnimation.start();
+        pageIndex++;
+        textView.setText(String.format("the page index is %s ", pageIndex));
+        textView.setTextColor(Color.BLACK);
+        LayoutParams layoutParams = new LayoutParams(MATCH_PARENT, MATCH_PARENT);
+        textView.setLayoutParams(layoutParams);
+        return textView;
+    }
+
+    private int[] getColors() {
+        if (bgColors == null) {
+            Resources resource = getResources();
+            bgColors = new int[]{
+                    resource.getColor(R.color.androidColorA),
+                    resource.getColor(R.color.androidColorB),
+                    resource.getColor(R.color.androidColorC),
+                    resource.getColor(R.color.androidColorD),
+                    resource.getColor(R.color.androidColorE),
+            };
+        }
+        return bgColors;
+    }
+
+    public void enableSwipeBack(View view) {
+        if (container.getChildCount() > 0) {
+            SwipeBackLayout swipeBackLayout = (SwipeBackLayout) container.getChildAt(pageIndex - 1);
+            swipeBackLayout.setSwipeBackEnable(!swipeBackLayout.getSwipeBackEnable());
+        }
+        updateEnableView();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (container.getChildCount() > 0) {
+            if (!isAnimating) {
+                ValueAnimator exitAnimation = ValueAnimator.ofFloat(0, screenWidth);
+                exitAnimation.setDuration(DURATION);
+                exitAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                        isAnimating = true;
+                        Float animatedValue = (Float) valueAnimator.getAnimatedValue();
+                        container.getChildAt(pageIndex - 1).setTranslationX(animatedValue);
+                    }
+                });
+                exitAnimation.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        isAnimating = false;
+                        container.removeViewAt(pageIndex - 1);
+                        pageIndex--;
+                        updateEnableView();
+                    }
+                });
+                exitAnimation.start();
+            }
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/elyria/swipeback/SwipeBackLayout.java
@@ -0,0 +1,261 @@
+package com.elyria.swipeback;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import static android.support.v4.widget.ViewDragHelper.DIRECTION_VERTICAL;
+import static android.support.v4.widget.ViewDragHelper.EDGE_LEFT;
+import static android.support.v4.widget.ViewDragHelper.STATE_DRAGGING;
+
+public class SwipeBackLayout extends FrameLayout {
+
+    private static final String TAG = "SwipeBackLayout";
+    // threshold
+    private static final float DEFAULT_SCROLL_THRESHOLD = 0.3f;
+    private float mScrollThreshold = DEFAULT_SCROLL_THRESHOLD;
+    private float mScrollPercent;
+    // scrim color
+    private static final int DEFAULT_SCRIM_COLOR = 0x99000000;
+    private int mScrimColor = DEFAULT_SCRIM_COLOR;
+    private double mScrimOpacity;
+    // content view
+    private View mReactRootView;
+    private int mReactRootViewLeft;
+    private int mReactRootViewTop;
+    private int mScreenWidth;
+    // flag
+    private boolean mEnable;
+    private boolean mInLayout;
+    // callback & helper
+    private ViewDragHelper mViewDragHelper;
+    private SwipeCallback mSwipeCallback;
+
+    public SwipeBackLayout(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public SwipeBackLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SwipeBackLayout(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mScreenWidth = context.getResources().getDisplayMetrics().widthPixels;
+        initView();
+    }
+
+    private void initView() {
+        mViewDragHelper = ViewDragHelper.create(this, 1f, new ViewDragHelper.Callback() {
+            // touch over the threshold of screen (0-0.3f area)
+            private boolean mIsScrollOverValid;
+
+            @Override
+            public boolean tryCaptureView(View child, int pointerId) {
+                boolean edgeTouched = mViewDragHelper.isEdgeTouched(EDGE_LEFT, pointerId);
+                mIsScrollOverValid = edgeTouched;
+                return edgeTouched && !mViewDragHelper.checkTouchSlop(DIRECTION_VERTICAL, pointerId);
+            }
+
+            @Override
+            public void onEdgeTouched(int edgeFlags, int pointerId) {
+                super.onEdgeTouched(edgeFlags, pointerId);
+                mViewDragHelper.captureChildView(mReactRootView, pointerId);
+            }
+
+            @Override
+            public int getViewHorizontalDragRange(View child) {
+                return mScreenWidth;
+            }
+
+            @Override
+            public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+                super.onViewPositionChanged(changedView, left, top, dx, dy);
+                mScrollPercent = Math.abs((float) left / (mReactRootView.getWidth()));
+                mReactRootViewLeft = left;
+                mReactRootViewTop = top;
+                invalidate();
+                if (mScrollPercent < mScrollThreshold && !mIsScrollOverValid) {
+                    mIsScrollOverValid = true;
+                }
+                if (mSwipeCallback != null) {
+                    mSwipeCallback.onScrollPercent(mScrollPercent);
+                    if (mViewDragHelper.getViewDragState() == STATE_DRAGGING
+                            && mScrollPercent >= mScrollThreshold && mIsScrollOverValid) {
+                        mIsScrollOverValid = false;
+                        mSwipeCallback.onScrollOverThreshold();
+                    }
+                }
+
+                if (mScrollPercent >= 1) {
+                    if (mSwipeCallback != null) {
+                        mSwipeCallback.onShouldFinish();
+                    }
+                }
+            }
+
+            @Override
+            public void onViewReleased(View releasedChild, float xvel, float yvel) {
+                final int childWidth = releasedChild.getWidth();
+                int left = xvel > 0 || xvel == 0 && mScrollPercent > mScrollThreshold ? childWidth : 0;
+                mViewDragHelper.settleCapturedViewAt(left, 0);
+                invalidate();
+            }
+
+            @Override
+            public int clampViewPositionHorizontal(View child, int left, int dx) {
+                return Math.min(child.getWidth(), Math.max(left, 0));
+            }
+        });
+        mViewDragHelper.setEdgeTrackingEnabled(EDGE_LEFT);
+    }
+
+    /**
+     * Set content view
+     */
+    public void setupReactRootView(View view) {
+        this.addView(view);
+        this.mReactRootView = view;
+    }
+
+    /**
+     * Set callback
+     */
+    public void setSwipeCallback(SwipeCallback swipeCallback) {
+        this.mSwipeCallback = swipeCallback;
+    }
+
+    /**
+     * Set enable or disable SwipeBack
+     */
+    public void setSwipeBackEnable(boolean enable) {
+        this.mEnable = enable;
+    }
+
+    /**
+     * Get SwipeBack's mEnable
+     */
+    public boolean getSwipeBackEnable() {
+        return this.mEnable;
+    }
+
+
+    /**
+     * Set scrim color
+     *
+     * @param color to use in 0xAARRGGBB format.
+     */
+    public void setScrimColor(int color) {
+        mScrimColor = color;
+        invalidate();
+    }
+
+    /**
+     * Set scroll threshold, close the activity or remove view when scrollPercent over this value
+     *
+     * @param threshold range of 0 ~ 1f
+     */
+    public void setScrollThreshold(float threshold) {
+        if (threshold >= 1.0f || threshold <= 0) {
+            throw new IllegalArgumentException("Threshold value should be between 0 and 1.0");
+        }
+        mScrollThreshold = threshold;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Log.d(TAG, String.format("dispatchTouchEvent() this is %s, enable = %s", this, mEnable));
+        return !mEnable || super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        Log.d(TAG, String.format("onInterceptTouchEvent() this is %s, enable = %s", this, mEnable));
+        return mEnable && mViewDragHelper.shouldInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, String.format("onTouchEvent() this is %s, enable = %s", this, mEnable));
+        if (!mEnable) return false;
+        mViewDragHelper.processTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        Log.d(TAG, String.format("onLayout() this is %s, enable = %s", this, mEnable));
+        mInLayout = true;
+        if (mReactRootView != null) {
+            mReactRootView.layout(mReactRootViewLeft, mReactRootViewTop,
+                    mReactRootViewLeft + mReactRootView.getMeasuredWidth(),
+                    mReactRootViewTop + mReactRootView.getMeasuredHeight());
+            mInLayout = false;
+        }
+    }
+
+    @Override
+    public void requestLayout() {
+        if (!mInLayout) {
+            super.requestLayout();
+        }
+    }
+
+    @Override
+    public void computeScroll() {
+        mScrimOpacity = 1 - mScrollPercent;
+        if (mViewDragHelper.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
+    }
+
+    @Override
+    protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
+        Log.d(TAG, String.format("drawChild() this is %s, enable = %s", this, mEnable));
+        final boolean drawContent = child == mReactRootView;
+        boolean ret = super.drawChild(canvas, child, drawingTime);
+        if (mScrimOpacity > 0 && drawContent
+                && mViewDragHelper.getViewDragState() != ViewDragHelper.STATE_IDLE) {
+            drawScrim(canvas, child);
+        }
+        return ret;
+    }
+
+    private void drawScrim(Canvas canvas, View child) {
+        final int baseAlpha = (mScrimColor & 0xff000000) >>> 24;
+        final int alpha = (int) (baseAlpha * mScrimOpacity);
+        final int color = alpha << 24 | (mScrimColor & 0xffffff);
+        canvas.clipRect(0, 0, child.getLeft(), getHeight());
+        canvas.drawColor(color);
+    }
+
+    public interface SwipeCallback {
+        /**
+         * Finish activity or remove view
+         */
+        void onShouldFinish();
+
+        /**
+         * The scroll percent of screen
+         *
+         * @param scrollPercent range of 0 ~ 1f
+         */
+        void onScrollPercent(float scrollPercent);
+
+        /**
+         * Called when scroll over threshold
+         *
+         * @see #mScrollThreshold
+         */
+        void onScrollOverThreshold();
+    }
+}
